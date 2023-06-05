75ffafa280b6472e0c14dade824397c4a21282d4
docluis <spam@luisgeissler.de>
2023-06-05 19:12:54 +0200

    hide xxe errors

--- service/www/index.php
+++ service/www/index.php
@@ -2,7 +2,7 @@
 require_once 'vendor/autoload.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_NOTICE);
 
 use Twig\Extension\SandboxExtension; // Enable sandbox mode
 use Twig\Sandbox\SecurityPolicy; // Enable sandbox mode
